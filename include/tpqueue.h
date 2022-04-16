2b6924539e260400eee99af6ec963bbcdf8e8d4e
Dmitry Volkov <83473625+Ocsidsus@users.noreply.github.com>
2022-04-16 11:58:54 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,11 +5,11 @@
 
 template<typename T, int size>
 class TPQueue {
-   private:
+    private:
     T arr[100];
     int first;
     int last;
-   public:
+    public:
     TPQueue() : first(0), last(0) { }
     void push(T x) {
         if (last - first >= size) {
